1330ef3ff30d91af3c57b39fa494c2e4dfd5e6f9
Rafael Zerpa <zrapar@gmail.com>
2019-05-14 15:01:12 -0400

    ready

--- js/slider-fotos.js/slider-script.js
+++ js/slider-fotos.js/slider-script.js
@@ -1,21 +1,40 @@
-$('#bootstrap-touch-slider').bsTouchSlider();
+// $('#bootstrap-touch-slider').bsTouchSlider();
 const options = {
     speed: 100,
     cursor: false,
     startDelay: 50,
 };
-let h1TypeInstances = new TypeIt('h1.typeIt', options);
-let pTypeInstances = new TypeIt('p.typeIt', options);
+let Instances = new TypeIt('.TypeIt', options);
+Instances.go();
+// let h1TypeInstances = new TypeIt('h1.typeIt', options);
+// let pTypeInstances = new TypeIt('p.typeIt', options);
 
-h1TypeInstances.go();
-pTypeInstances.go();
-$('#bootstrap-touch-slider').on('slide.bs.carousel', function () {
-    h1TypeInstances.reset().freeze();
-    pTypeInstances.reset().freeze();
+// h1TypeInstances.go();
+// pTypeInstances.go();
 
+// $('#bootstrap-touch-slider').on('slide.bs.carousel', function () {
+//     h1TypeInstances.reset().freeze();
+//     pTypeInstances.reset().freeze();
+//
+//     setTimeout(() => {
+//         h1TypeInstances.unfreeze();
+//         pTypeInstances.unfreeze();
+//
+//     }, 900);
+// });
+
+
+// On before slide change
+$('.start-screen__slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    Instances.reset().freeze();
     setTimeout(() => {
-        h1TypeInstances.unfreeze();
-        pTypeInstances.unfreeze();
+        Instances.unfreeze();
+    }, 900);
+});
 
+$('.start-screen__slider').on('swipe', function(event, slick, direction){
+    Instances.reset().freeze();
+    setTimeout(() => {
+        Instances.unfreeze();
     }, 900);
 });
