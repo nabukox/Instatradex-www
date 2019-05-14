1b1b30aa974e6bb482b6bb9660e41e346cdb847b
Rafael Zerpa <zrapar@gmail.com>
2019-05-14 10:58:24 -0400

    Type It fixed

--- js/slider-fotos.js/slider-script.js
+++ js/slider-fotos.js/slider-script.js
@@ -1,32 +1,21 @@
-function getBootstrapDeviceSize() {
-    return $('#users-device-size').find('div:visible').first().attr('id');
-}
-console.log(getBootstrapDeviceSize());
 $('#bootstrap-touch-slider').bsTouchSlider();
 const options = {
     speed: 100,
     cursor: false,
     startDelay: 50,
 };
-let h1TypeInstances = new TypeIt('h1', options);
-let pTypeInstances = new TypeIt('p', options);
-let titleInstace = new TypeIt('#element',{
-    strings: ['Welcome to the Sliders'],
-    speed: 100,
-    cursor: false,
-    breakLines: false,
-});
-titleInstace.go();
+let h1TypeInstances = new TypeIt('h1.typeIt', options);
+let pTypeInstances = new TypeIt('p.typeIt', options);
+
 h1TypeInstances.go();
 pTypeInstances.go();
 $('#bootstrap-touch-slider').on('slide.bs.carousel', function () {
     h1TypeInstances.reset().freeze();
     pTypeInstances.reset().freeze();
-    titleInstace.reset().freeze();
+
     setTimeout(() => {
         h1TypeInstances.unfreeze();
         pTypeInstances.unfreeze();
-        titleInstace.unfreeze();
 
     }, 900);
 });
